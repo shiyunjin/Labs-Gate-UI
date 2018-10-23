46bdcd6d690e088d22b468d5892a602fa6bdaa41
shiyunjin <admin@shiyunjin.cn>
2018-10-23 14:01:22 +0800

    fix(index): change title

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ export default class Logo extends PureComponent {
     return (
       <div className="logo" style={{}}>
         <Link to="/" className="logo-text">
-          LOGO
+          School Network
         </Link>
       </div>
     );
